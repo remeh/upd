d079b4aa9dd3c0e6763c2dc5c4c23e01b821b3e8
remeh <me@remy.io>
2015-02-15 18:31:18 +0100

    serving: resize feature for png/jpg.

--- src/server/serving_handler.go
+++ src/server/serving_handler.go
@@ -4,12 +4,18 @@
 package server
 
 import (
+	"bytes"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/nfnt/resize"
 )
 
 const (
@@ -72,9 +78,79 @@ func (s *ServingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// detect the content-type
 	contentType := http.DetectContentType(data)
+
+	// we'll see whether or not we want to generate a thumbnail
+	r.ParseForm()
+	width := r.Form.Get("w")
+	height := r.Form.Get("h")
+	if len(width) != 0 && len(height) != 0 {
+		iwidth, err := strconv.Atoi(width)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		iheight, err := strconv.Atoi(height)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+
+		data = s.Resize(id, contentType, data, uint(iwidth), uint(iheight))
+	}
+
 	w.Header().Set("Content-Type", contentType)
 	w.Header().Set(HEADER_ORIGINAL_FILENAME, entry.Original)
 	w.Header().Set("Content-Disposition", "inline; filename*=UTF-8''"+url.QueryEscape(entry.Original))
 	w.Write(data)
+}
+
+func (s *ServingHandler) Resize(id string, contentType string, data []byte, width uint, height uint) []byte {
+	if len(data) == 0 {
+		return data
+	}
+	if contentType != "image/png" && contentType != "image/jpeg" {
+		return data
+	}
+
+	var err error
+	var img image.Image
+	var buffer *bytes.Buffer
+
+	// create the Image instance
+	if contentType == "image/png" {
+		img, err = png.Decode(bytes.NewReader(data))
+		if err != nil {
+			log.Println("[err] Can't resize png image with id:", id)
+			return data
+		}
+	} else if contentType == "image/jpeg" {
+		img, err = jpeg.Decode(bytes.NewReader(data))
+		if err != nil {
+			log.Println("[err] Can't resize jpg image with id:", id)
+			return data
+		}
+	}
+
+	// resize the image
+	img = resize.Resize(width, height, img, resize.Lanczos3)
+
+	// write the data
+	buffer = bytes.NewBuffer(nil)
+	if contentType == "image/png" {
+		err = png.Encode(buffer, img)
+		if err != nil {
+			log.Println("[err] Can't encode png image with id:", id)
+			return data
+		}
+	} else if contentType == "image/jpeg" {
+		err = jpeg.Encode(buffer, img, nil)
+		if err != nil {
+			log.Println("[err] Can't encode jpg image with id:", id)
+			return data
+		}
+	}
+
+	return buffer.Bytes()
 }
